a80bc392fed3c4e2d231a8351c789435c857e57e
= <=>
2018-12-09 09:21:26 +0700

    ip

---
+++ gokisoft/lib/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('backend.index');
+});
+// Route::group(['prefix' => 'admin'], function() {
+// 	Route::get('/', 'HomeController@getHome');
+// });
+
+// Route::get('/', 'HomeController@getHome');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
